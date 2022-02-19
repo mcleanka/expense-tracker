cd1bf59646fcc4cea2ebd2a1852cf0a3484172e6
mcleanka <mcleankasambala@gmail.com>
2022-02-19 22:01:42 +0200

    added routes

--- api/routes/api.php
+++ api/routes/api.php
@@ -1,7 +1,14 @@
 <?php
 
+use App\Http\Controllers\ExpenseController;
+use App\Http\Controllers\IncomeController;
+use App\Http\Controllers\LoanController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+
+Route::resource(IncomeController::class)->names('income');
+Route::resource(ExpenseController::class)->names('expense');
+Route::resource(LoanController::class)->names('loan');
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
