1970c56e13b5bda7dde321b60ca7b3776c2723eb
mcleanka <mcleankasambala@gmail.com>
2022-02-20 12:25:29 +0200

    added relations

--- api/app/Http/Resources/IncomeResource.php
+++ api/app/Http/Resources/IncomeResource.php
@@ -17,10 +17,16 @@ class IncomeResource extends JsonResource
         return [
             'id' => $this->id,
             'name' => $this->name,
-            'detail' => $this->detail,
-            'created_at' => $this->created_at->format('d/m/Y'),
-            'updated_at' => $this->updated_at->format('d/m/Y'),
+            'user' => $this->user->only([
+                'name',
+                'email',
+            ]),
+            'description' => $this->description,
+            'owner' => $this->owner,
+            'amount' => $this->amount,
+            'date' => $this->date->format('jS F, Y'),
+            'created_at' => $this->created_at->format('jS F, Y'),
+            'updated_at' => $this->updated_at->format('jS F, Y'),
         ];
-
     }
 }
